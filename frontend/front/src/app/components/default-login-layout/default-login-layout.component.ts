9a4cab78cb8dc5df87457789ec5d2a662a4dc5c3
Leandrolm <leandromelgarejo@hotmail.com>
2025-06-10 11:38:55 -0300

    create login page

--- frontend/front/src/app/components/default-login-layout/default-login-layout.component.ts
+++ frontend/front/src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
@@ -10,7 +10,19 @@ import {MatDividerModule} from '@angular/material/divider';
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
-  @Input() title: string = '';
-  @Input() primaryButtonText: string = '';
-  @Input() secondaryButtonText: string = '';
+  @Input() title: string = "";
+  @Input() primaryButtonText: string = "";
+  @Input() secondaryButtonText: string = "";
+  @Input() disablePrimaryBtn: boolean = true;
+  @Output("submit") onSubmit = new EventEmitter();
+
+  @Output("navigate") onNavigate = new EventEmitter();
+
+  submit(){
+    this.onSubmit.emit();
+  }
+
+  navigate(){
+    this.onNavigate.emit();
+  }
 }
--- frontend/front/src/app/pages/login/login.component.ts
+++ frontend/front/src/app/pages/login/login.component.ts
@@ -1,39 +1,52 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primare-input/primare-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
+
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
+
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PrimaryInputComponent
+  ],
+  providers: [
+    LoginService
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  loginForm = new FormGroup({
-    username: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
-  });
+  loginForm!: FormGroup<LoginForm>;
 
-  onSubmit() {
-    if (this.loginForm.valid) {
-      const loginData = this.loginForm.value;
-      console.log('Login attempt with:', loginData);
-      // Chame aqui seu serviço de autenticação
-    } else {
-      this.loginForm.markAllAsTouched();
-    }
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
   }
 
-  onSignUp() {
-    console.log('Navigate to sign up page');
-    // Lógica para redirecionar ao cadastro
+  submit(){
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Login successful!"),
+      error: () => this.toastService.error("Unexpected error! Please try again later")
+    })
+  }
+
+  navigate(){
+    this.router.navigate(["signup"])
   }
 }
-
